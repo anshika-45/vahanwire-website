0c7e94194f59cf9a31402af3e074513e7ce966bc
anshika <anshika@vahanwire.com>
2025-11-19 13:50:06 +0530

    chnge

--- src/pages/ServiceCenter.jsx
+++ src/pages/ServiceCenter.jsx
@@ -1,44 +1,19 @@
 import React from "react";
 import ServiceCenterBanner from "../assets/ServiceCenter.svg";
-import "../index.css";
+import PageBanner from "../components/PageBanner";
 
 const ServiceCenter = () => {
   return (
-    <section
-      className="relative w-full overflow-hidden bg-gray-900 h-[40vh] sm:h-[40vh] md:h-[60vh] lg:h-[70vh]"
-      style={{
-        backgroundImage: `url(${ServiceCenterBanner})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        willChange: 'background-image',
-        contentVisibility: 'auto',
-      }}
-    >
-      <div className="absolute inset-0 bg-black/60 pointer-events-none" />
-    
-      <div className="relative z-10">
-        <div className="ticker-container">
-          <div className="ticker">
-            <div className="ticker__move">
-              {[...Array(15)].map((_, i) => (
-                <span
-                  key={i}
-                  className="ticker-item text-xs sm:text-sm md:text-base font-normal"
-                >
-                  Coming Soon&nbsp;&nbsp;|&nbsp;&nbsp;
-                </span>
-              ))}
-            </div>
-          </div>
-        </div>
+    <>
+      <div>
+        <PageBanner
+          title="Coming Soon"
+          image={ServiceCenterBanner}
+          useGradientTitle={true}
+        />
+        <div className="mt-6 md:mt-12 lg:mt-12 xl:mt-0" />
       </div>
-    
-      <div className="relative z-10 flex items-center justify-center h-[40vh] sm:h-[40vh] md:h-[60vh] lg:h-[70vh]">
-        <h1 className="text-center leading-tight banner-title gradient-text text-4xl sm:text-5xl md:text-6xl font-semibold">
-          Coming Soon
-        </h1>
-      </div>
-    </section>
+    </>
   );
 };
 
