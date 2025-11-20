e9c1c45ee689fbac33c6bcd247bb34425738ff9b
jaswant <jaswant@vahanwire.com>
2025-11-20 15:29:18 +0530

    remove hard navigation

--- src/pages/VehicleAmcFilter.jsx
+++ src/pages/VehicleAmcFilter.jsx
@@ -100,19 +100,6 @@ const VehicleAmcFilter = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [showPopup, setShowPopup] = useState(null);
 
-  useEffect(() => {
-    const handlePopState = (event) => {
-      event.preventDefault();
-      navigate('/vehicle-amc', { replace: true });
-    };
-
-    window.history.pushState(null, '', window.location.pathname);
-    window.addEventListener('popstate', handlePopState);
-
-    return () => {
-      window.removeEventListener('popstate', handlePopState);
-    };
-  }, [navigate]);
 
   useEffect(() => {
     if (locationVehicle && locationPlans) {
